38b06513c2b9b0bd9f72bc676af13b3606b26e4b
julespatry <julespatry@gmail.com>
2022-08-11 12:25:43 -0400

    adds rough draft example

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const Server = require('socket.io');
-const io = Server(3001);
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const _http = http.Server(app);
+// const server = http.createServer(app);
+
+// server.listen('3001', () => {
+//   const msg = `v${packageJson.version} Server running in "${FP_ENVIRONMENT}" at: http://${HTTP_HOST}:${port}`;
+//   slackMessage({ channel: CHANNELS.deployment, message: msg, logger, alert: true });
+// });
+
+const io = require('socket.io')(_http, { cors: { origin: '*' } });
 io.on('connection', (socket) => {
   console.log('hit');
   socket.emit('hello', 'world');
